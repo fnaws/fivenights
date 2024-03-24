954b8a9a157119a70cd197cb04aad21b30f4b6be
Kenny Zhang <kjz5ape@virginia.edu>
2024-03-24 00:36:44 -0400

    added 5-day graph

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index():
 
     # graphJSON = json.dumps(plotting_data, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('index.html', plotting_data=json.dumps(plotting_data))
+    return render_template('index.html', data=data)
             
 if __name__ == '__main__':
     app.run(debug=True)
